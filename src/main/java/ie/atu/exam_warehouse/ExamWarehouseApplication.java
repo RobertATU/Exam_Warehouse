946a2cf57d4331cccc399368df8841e58cafe7e9
G00395612@atu.ie <G00395612@atu.ie>
2023-11-08 09:23:47 +0000

    Initial commit

---
+++ src/main/java/ie/atu/exam_warehouse/ExamWarehouseApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.exam_warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/exam_warehouse/ExamWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.exam_warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
