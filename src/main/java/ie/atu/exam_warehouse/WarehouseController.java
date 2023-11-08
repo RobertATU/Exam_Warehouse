c1ae8202366ecb544a1ecd6a89f933f3da8d384f
G00395612@atu.ie <G00395612@atu.ie>
2023-11-08 10:29:15 +0000

    Confirmed warehouse service checks id and sends object to product service

--- src/main/java/ie/atu/exam_warehouse/WarehouseController.java
+++ src/main/java/ie/atu/exam_warehouse/WarehouseController.java
@@ -5,21 +5,34 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class WarehouseController {
     private ObjectInterface objectInterface;
     private WarehouseObject warehouseObject;
+    private List<WarehouseObject>stock;
     public WarehouseController(ObjectInterface objectInterface){
         this.objectInterface = objectInterface;
         this.warehouseObject =  new WarehouseObject();
+       this.stock =  new ArrayList<>();
         warehouseObject.setWarehouseId(201);
         warehouseObject.setCapacity(100);
         warehouseObject.setLocation("London");
+        stock.add(warehouseObject);
     }
 
-   @PostMapping("/getWarehouseById")
-    WarehouseObject getWarehouseById(){
-        return warehouseObject;
+   @PostMapping("/addProduct")
+    WarehouseObject getWarehouseById(@RequestBody WarehouseObject sent){
+        WarehouseObject check = new WarehouseObject();
+        for(WarehouseObject stock1:stock){
+            if ((stock1.getWarehouseId() == sent.getWarehouseId())){{
+                check = warehouseObject;
+            }
+            }
+        }
+        return check;
     }
 
     @GetMapping("/Test")
